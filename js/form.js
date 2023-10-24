d0d647ecd161098927f0bd3dc48205367e219cb1
Olha-Kutsybala <olyasheremeta@gmail.com>
2023-10-24 20:02:36 +0300

    form

---
+++ js/form.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('.form_telegram');
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  sendMessage(form);
+});
+
+async function sendMessage(form) {
+  const formData = new FormData(form);
+  if (formData) {
+    const url = 'sendmessage.php';
+    const response = await fetch(url, {
+      method: 'POST',
+      body: formData,
+    });
+    if (response.ok) {
+      form.reset();
+      alert('Form sent!');
+    } else {
+      alert('Error');
+    }
+  }
+}
---
+++ sendmessage.php
@@ -0,0 +1,18 @@
+<?php
+$content = '';
+foreach($_POST as $key => $value){
+    if($value){
+        $content .= "<b>$key</b>: <i>$value</i>\n";
+    }
+}
+if(trim($content)){
+    $content = "<b>Message from Site</b>\n".$content;
+    $apiToken = "6667312532:AAGbSX8Er9D7YQnTFX5u6QnqyL_S40cnRok";
+    $data = [
+        'chat_id' => '@testMusicSchool2Bot',
+        'text' => $content,
+        'parse_mode' => 'HTML'
+    ];
+    $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?"http_build_query($data))
+}
+?>
